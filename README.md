ab3d18b916b1ec6f9f1ed26f2e050edf26818ab2
肖念 <xiaonian0430@163.com>
2022-07-15 07:52:05 +0800

    chat demo

---
+++ README.md
@@ -0,0 +1,14 @@
+# 聊天系统demo
+
+基于 SwooleGateway 开发的一个群聊demo，供学习使用
+
+[【Github】](https://github.com/xiaonian0430/demo_server_php_swoole_chat.git)
+
+## 安装
+
+1. 下载本仓库源码
+2. 执行`composer install`
+3. 配置 gateway.php 中的监听地址
+4. 修改 index.html 中的 websocket 连接地址
+5. 启动 register worker gateway 三个服务
+
---
+++ event_worker.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+use Swoole\Timer;
+use Xielei\Swoole\Api;
+
+use Xielei\Swoole\Helper\WorkerEvent as HelperWorkerEvent;
+
+class WorkerEvent extends HelperWorkerEvent
+{
+    public function onWorkerStart()
+    {
+        if ($this->worker->getServer()->worker_id == 0) {
+            $this->xtimer = Timer::tick(5000, function () {
+                Api::sendToAll(json_encode([
+                    'event' => 'groupList',
+                    'data' => $this->getGroupList(),
+                ]));
+            });
+        }
+    }
+
+    public function onWorkerExit()
+    {
+        if (isset($this->xtimer)) {
+            Timer::clear($this->xtimer);
+        }
+    }
+
+    public function onConnect(string $client, array $session)
+    {
+        Api::sendToClient($client, json_encode([
+            'event' => 'connect',
+            'data' => $client,
+        ]));
+    }
+
+    public function onReceive(string $client, array $session, string $data)
+    {
+        try {
+            if (substr($data, 0, 1) !== '{') {
+                return;
+            }
+            $data = json_decode($data, true);
+            $event = $data['event'];
+            $cmd = $data['cmd'];
+            $param = $data['param'];
+            switch ($cmd) {
+                case 'getGroupListInfo':
+                    $res = $this->getGroupList();
+                    break;
+
+                default:
+                    $res = Api::$cmd(...$param);
+                    break;
+            }
+            if ($event) {
+                if ($res instanceof Iterator) {
+                    $res = iterator_to_array($res);
+                }
+                Api::sendToClient($client, json_encode([
+                    'event' => $event,
+                    'data' => $res,
+                ]));
+            }
+        } catch (\Throwable $th) {
+            Api::sendToClient($client, json_encode([
+                'event' => 'error',
+                'data' => $th->getMessage(),
+            ]));
+        }
+    }
+
+    public function onClose(string $client, array $session, array $bind)
+    {
+        Api::sendToAll(json_encode([
+            'event' => 'close',
+            'data' => [
+                'client' => $client,
+                'group_list' => $bind['group_list'],
+            ],
+        ]));
+    }
+
+    private function getGroupList(): array
+    {
+        $res = [
+            '大厅' => 0,
+        ];
+        foreach (Api::getGroupList(true) as $group) {
+            $res[$group] = Api::getClientCountByGroup($group);
+        }
+        return $res;
+    }
+}
